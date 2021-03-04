0ab43db948904c2739cc69c8cd73de9f846610d3
1DAM <1DAM@DESKTOP-73K3BKO>
2021-03-04 19:31:47 +0100

    Primer commit

---
+++ src/main/java/Practica4AD/xQuery/App.java
@@ -0,0 +1,13 @@
+package Practica4AD.xQuery;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
